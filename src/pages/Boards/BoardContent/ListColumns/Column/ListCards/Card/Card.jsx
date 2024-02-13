77b77e34e40fa50fd6d31f7ccaf9eb2b6a9c0752
duysong435 <duysong435@gmail.com>
2024-02-13 17:01:31 +0700

    fix bug empty column

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -41,7 +41,10 @@ function Card({ card }) {
       sx={{
         cursor: 'pointer',
         boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
-        overflow: 'unset'
+        overflow: 'unset',
+        display: card?.FE_PlaceholaderCard ? 'none' : 'block'
+        // overflow: card?.FE_PlaceholaderCard ? 'hidden' : 'unset',
+        // height: card?.FE_PlaceholaderCard ? '0px' : 'unset'
       }}>
 
       {card?.cover &&
--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -3,4 +3,17 @@ export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
-
+// Cách fix bug logic thư viện DnD-kit khi column là rỗng:
+// Phía FE sẽ tự tạo ra một cái card đặc biệt: Placeholder Card, không liên quan đến Back-end
+// Card đặc biệt này sẽ được  ẩn  ở giao diện UI người dùng
+// Cấu trúc Id của cái Id card này để Unique rất đơn giản, không cần phải lmaf random phức tạp:
+// "columnId-placeholder-card" (mỗi column chỉ có thể tối đa một cái Placeholder Card)
+// Quan trọng khi tạo: phải đầy đủ: (_id, boardId, columnId, FE_placeholdder)
+export const generatePlaceholderCard = (column) => {
+  return {
+    _id: `${column._id}-placeholder-card`,
+    boardId: column.boardId,
+    columnId: column._id,
+    FE_PlaceholaderCard: true
+  }
+}
