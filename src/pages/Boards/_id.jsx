10e02d2e90b4f6858dd08350d81655f15fa9ec6a
duysong435 <duysong435@gmail.com>
2024-02-23 16:26:56 +0700

    axios call api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -6,9 +6,20 @@ import AppBar from '~/components/AppBar/AppBoard'
 import BoardBar from './BoardsBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardid = '65d85bf59413865fa34cd6cb'
+    fetchBoardDetailsAPI(boardid).then((board) => {
+      setBoard(board)
+    })
+  }, [])
+  console.log(board)
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
