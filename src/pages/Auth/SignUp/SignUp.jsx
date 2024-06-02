92b29e7e095f6a849bac626e2b4038cd5cfccef5
Shine <meotoameo@gmail.com>
2024-06-02 14:33:01 +0700

    fixvecel

--- src/pages/Auth/SignUp/SignUp.jsx
+++ src/pages/Auth/SignUp/SignUp.jsx
@@ -16,7 +16,7 @@ import RightBG from '~/assets/right-bg.svg'
 import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
 import SvgIcon from '@mui/material/SvgIcon'
 import { styled } from '@mui/material/styles'
-import { ReactComponent as Google } from '~/assets/Google.svg'
+import { ReactComponent as Google } from '~/assets/google.svg'
 import LogoAlassion from '~/assets/logo-alassian.svg'
 import { useEffect } from 'react'
 import axios from 'axios'
@@ -37,7 +37,7 @@ export default function SignUp() {
 
   const dispatch = useDispatch()
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     data.name = data.firstName + ' ' + data.lastName
     delete data.firstName
     delete data.lastName
@@ -93,7 +93,7 @@ export default function SignUp() {
           component="form"
           onSubmit={handleSubmit(onSubmit)}
           noValidate
-          sx={theme => ({
+          sx={(theme) => ({
             //  bgcolor: 'black',
             display: 'flex',
             flexDirection: 'column',
@@ -104,20 +104,13 @@ export default function SignUp() {
           })}
         >
           <Box sx={{ display: 'flex' }}>
-            <SvgIcon
-              component={TrelloIcon}
-              fontSize="large"
-              inheritViewBox
-              sx={{ color: '#0052cc' }}
-            />
+            <SvgIcon component={TrelloIcon} fontSize="large" inheritViewBox sx={{ color: '#0052cc' }} />
 
             <Typography component="h1" variant="h5" sx={{ fontWeight: '800' }}>
               Trello
             </Typography>
           </Box>
-          <Typography sx={{ fontWeight: '500', p: 2 }}>
-            Sign up to Continue
-          </Typography>
+          <Typography sx={{ fontWeight: '500', p: 2 }}>Sign up to Continue</Typography>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -134,16 +127,8 @@ export default function SignUp() {
                   maxLength: 100
                 })}
               />
-              {errors?.firstName?.type === 'required' && (
-                <Typography sx={{ color: 'red' }}>
-                  This field is required
-                </Typography>
-              )}
-              {errors?.firstName?.type === 'maxLength' && (
-                <Typography sx={{ color: 'red' }}>
-                  Email cannot exceed 100 characters
-                </Typography>
-              )}
+              {errors?.firstName?.type === 'required' && <Typography sx={{ color: 'red' }}>This field is required</Typography>}
+              {errors?.firstName?.type === 'maxLength' && <Typography sx={{ color: 'red' }}>Email cannot exceed 100 characters</Typography>}
             </Grid>
 
             <Grid item xs={12} sm={6}>
@@ -160,16 +145,8 @@ export default function SignUp() {
                   maxLength: 100
                 })}
               />
-              {errors?.lastName?.type === 'required' && (
-                <Typography sx={{ color: 'red' }}>
-                  This field is required
-                </Typography>
-              )}
-              {errors?.lastName?.type === 'maxLength' && (
-                <Typography sx={{ color: 'red' }}>
-                  Email cannot exceed 100 characters
-                </Typography>
-              )}
+              {errors?.lastName?.type === 'required' && <Typography sx={{ color: 'red' }}>This field is required</Typography>}
+              {errors?.lastName?.type === 'maxLength' && <Typography sx={{ color: 'red' }}>Email cannot exceed 100 characters</Typography>}
             </Grid>
 
             <Grid item xs={12}>
@@ -188,19 +165,9 @@ export default function SignUp() {
                   pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
                 })}
               />
-              {errors?.email?.type === 'required' && (
-                <Typography sx={{ color: 'red' }}>
-                  This field is required
-                </Typography>
-              )}
-              {errors?.email?.type === 'maxLength' && (
-                <Typography sx={{ color: 'red' }}>
-                  Email cannot exceed 100 characters
-                </Typography>
-              )}
-              {errors?.email?.type === 'pattern' && (
-                <Typography sx={{ color: 'red' }}>example@gmail.com</Typography>
-              )}
+              {errors?.email?.type === 'required' && <Typography sx={{ color: 'red' }}>This field is required</Typography>}
+              {errors?.email?.type === 'maxLength' && <Typography sx={{ color: 'red' }}>Email cannot exceed 100 characters</Typography>}
+              {errors?.email?.type === 'pattern' && <Typography sx={{ color: 'red' }}>example@gmail.com</Typography>}
             </Grid>
 
             <Grid item xs={12}>
@@ -220,58 +187,28 @@ export default function SignUp() {
                   // pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/
                 })}
               />
-              {errors?.password?.type === 'required' && (
-                <Typography sx={{ color: 'red' }}>
-                  This field is required
-                </Typography>
-              )}
-              {errors?.password?.type === 'maxLength' && (
-                <Typography sx={{ color: 'red' }}>
-                  Password cannot exceed 25 characters
-                </Typography>
-              )}
-              {errors?.password?.type === 'minLength' && (
-                <Typography sx={{ color: 'red' }}>
-                  Password must not be less than 6 characters
-                </Typography>
-              )}
+              {errors?.password?.type === 'required' && <Typography sx={{ color: 'red' }}>This field is required</Typography>}
+              {errors?.password?.type === 'maxLength' && <Typography sx={{ color: 'red' }}>Password cannot exceed 25 characters</Typography>}
+              {errors?.password?.type === 'minLength' && <Typography sx={{ color: 'red' }}>Password must not be less than 6 characters</Typography>}
             </Grid>
             <Grid item xs={12}>
-              <Typography
-                variant="body2"
-                sx={{ textAlign: 'center', fontSize: '11px', marginTop: '8px' }}
-              >
-                By signing up, I accept the Atlassian Cloud Terms of Service and
-                acknowledge the Privacy Policy.
+              <Typography variant="body2" sx={{ textAlign: 'center', fontSize: '11px', marginTop: '8px' }}>
+                By signing up, I accept the Atlassian Cloud Terms of Service and acknowledge the Privacy Policy.
               </Typography>
             </Grid>
           </Grid>
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2, bgcolor: '#0052cc' }}
-          >
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2, bgcolor: '#0052cc' }}>
             Sign up
           </Button>
           <Typography sx={{ color: 'grey' }}>Or continue with:</Typography>
           <GoogleButton fullWidth color="text" sx={{ mt: 2, mb: 2 }}>
-            <SvgIcon
-              component={Google}
-              fontSize="medium"
-              inheritViewBox
-              sx={{ padding: '2px' }}
-            />
+            <SvgIcon component={Google} fontSize="medium" inheritViewBox sx={{ padding: '2px' }} />
             <Typography>Google</Typography>
           </GoogleButton>
           <Grid container justifyContent="center" sx={{ marginBottom: '8px' }}>
             <Grid item>
-              <Link
-                href={path.SignIn}
-                variant="body2"
-                sx={{ textDecoration: 'none' }}
-              >
+              <Link href={path.SignIn} variant="body2" sx={{ textDecoration: 'none' }}>
                 Already have an account? Sign in
               </Link>
             </Grid>
@@ -285,14 +222,8 @@ export default function SignUp() {
               width: '100%'
             }}
           >
-            <img
-              src={LogoAlassion}
-              style={{ marginTop: '30px', width: '160px' }}
-            />
-            <Typography
-              variant="body2"
-              sx={{ textAlign: 'center', fontSize: '11px', paddingTop: '8px' }}
-            >
+            <img src={LogoAlassion} style={{ marginTop: '30px', width: '160px' }} />
+            <Typography variant="body2" sx={{ textAlign: 'center', fontSize: '11px', paddingTop: '8px' }}>
               One account for Trello, Jira, Confluence and{' '}
               <Link href="#" sx={{ textDecoration: 'none' }}>
                 more
@@ -300,29 +231,17 @@ export default function SignUp() {
               .
             </Typography>
             <Box sx={{ marginTop: '12px', display: 'flex' }}>
-              <Typography
-                variant="body2"
-                sx={{ textAlign: 'center', fontSize: '11px' }}
-              >
+              <Typography variant="body2" sx={{ textAlign: 'center', fontSize: '11px' }}>
                 Privacy Policy
               </Typography>
-              <Typography
-                variant="body2"
-                sx={{ textAlign: 'center', fontSize: '11px', padding: '0 5px' }}
-              >
+              <Typography variant="body2" sx={{ textAlign: 'center', fontSize: '11px', padding: '0 5px' }}>
                 •
               </Typography>
-              <Typography
-                variant="body2"
-                sx={{ textAlign: 'center', fontSize: '11px' }}
-              >
+              <Typography variant="body2" sx={{ textAlign: 'center', fontSize: '11px' }}>
                 User Notice
               </Typography>
             </Box>
-            <Typography
-              variant="body2"
-              sx={{ textAlign: 'center', fontSize: '11px', marginTop: '8px' }}
-            >
+            <Typography variant="body2" sx={{ textAlign: 'center', fontSize: '11px', marginTop: '8px' }}>
               This site is protected by reCAPTCHA and the Google{' '}
               <Link href="#" sx={{ textDecoration: 'none' }}>
                 Privacy Policy
